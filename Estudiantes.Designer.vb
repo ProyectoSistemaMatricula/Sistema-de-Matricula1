201c6e4be1c6f646e059de854e0cb1b06e8c6646
user38 <user38@DESKTOP-JS10T0S>
2025-01-17 18:38:36 -0600

    agregar un comentario y una solucion

--- Estudiantes.Designer.vb
+++ Estudiantes.Designer.vb
@@ -22,9 +22,28 @@ Partial Class Estudiantes
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Estudiantes"
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(335, 125)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "hello"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Estudiantes
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Name = "Estudiantes"
+        Text = "Estudiantes"
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
