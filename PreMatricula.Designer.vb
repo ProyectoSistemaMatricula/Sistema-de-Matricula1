cad93208609a32b531024d217ba2624ae7f0e006
Emmanuel Castillo <emmanuelfernando930@gmail.com>
2025-01-20 10:44:59 -0600

    AGREGAR UN COMENTARIO Y UNA INSTRUCCION

--- PreMatricula.Designer.vb
+++ PreMatricula.Designer.vb
@@ -22,9 +22,132 @@ Partial Class PreMatricula
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "PreMatricula"
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(256, 46)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(186, 31)
+        Label1.TabIndex = 0
+        Label1.Text = "PREMATRICULA"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(86, 118)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(159, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "NOMBRE DE ALUMNO"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(86, 185)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(113, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "ID DE ALUMNO"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(86, 250)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(155, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "CORREO DE ALUMNO"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(151, 331)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(105, 29)
+        Button1.TabIndex = 4
+        Button1.Text = "CONFIRMAR"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(317, 331)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 5
+        Button2.Text = "GUARDAR"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(481, 331)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 6
+        Button3.Text = "CANCELAR"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(256, 115)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(201, 27)
+        TextBox1.TabIndex = 7
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(256, 185)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(201, 27)
+        TextBox2.TabIndex = 8
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(271, 250)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(201, 27)
+        TextBox3.TabIndex = 9
+        ' 
+        ' PreMatricula
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "PreMatricula"
+        Text = "V"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
 End Class
