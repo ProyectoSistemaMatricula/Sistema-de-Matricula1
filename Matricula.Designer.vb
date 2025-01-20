b7ff53aaa6113baf810e96571c6c0d96543e8932
marlin184 <marlin.lagos@uph.edu.hn>
2025-01-20 17:53:51 -0600

    modificar

--- Matricula.Designer.vb
+++ Matricula.Designer.vb
@@ -25,7 +25,6 @@ Partial Class Matricula
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Matricula))
         GroupBox1 = New GroupBox()
         ComboBox1 = New ComboBox()
-        CheckedListBox1 = New CheckedListBox()
         Label10 = New Label()
         Label9 = New Label()
         Label8 = New Label()
@@ -58,10 +57,6 @@ Partial Class Matricula
         ' 
         ' GroupBox1
         ' 
-        GroupBox1.Controls.Add(ComboBox1)
-        GroupBox1.Controls.Add(CheckedListBox1)
-        GroupBox1.Controls.Add(Label10)
-        GroupBox1.Controls.Add(Label9)
         GroupBox1.Controls.Add(Label8)
         GroupBox1.Controls.Add(TextBox7)
         GroupBox1.Controls.Add(DateTimePicker1)
@@ -80,9 +75,11 @@ Partial Class Matricula
         GroupBox1.Controls.Add(Label1)
         GroupBox1.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         GroupBox1.ForeColor = Color.Black
-        GroupBox1.Location = New Point(12, 64)
+        GroupBox1.Location = New Point(10, 48)
+        GroupBox1.Margin = New Padding(3, 2, 3, 2)
         GroupBox1.Name = "GroupBox1"
-        GroupBox1.Size = New Size(808, 245)
+        GroupBox1.Padding = New Padding(3, 2, 3, 2)
+        GroupBox1.Size = New Size(707, 155)
         GroupBox1.TabIndex = 0
         GroupBox1.TabStop = False
         GroupBox1.Text = "Información del estudiante"
@@ -90,27 +87,19 @@ Partial Class Matricula
         ' ComboBox1
         ' 
         ComboBox1.FormattingEnabled = True
-        ComboBox1.Location = New Point(143, 195)
+        ComboBox1.Location = New Point(92, 287)
+        ComboBox1.Margin = New Padding(3, 2, 3, 2)
         ComboBox1.Name = "ComboBox1"
-        ComboBox1.Size = New Size(246, 31)
+        ComboBox1.Size = New Size(174, 23)
         ComboBox1.TabIndex = 18
-        ' 
-        ' CheckedListBox1
-        ' 
-        CheckedListBox1.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        CheckedListBox1.FormattingEnabled = True
-        CheckedListBox1.Location = New Point(506, 195)
-        CheckedListBox1.Name = "CheckedListBox1"
-        CheckedListBox1.Size = New Size(296, 26)
-        CheckedListBox1.TabIndex = 7
         ' 
         ' Label10
         ' 
         Label10.AutoSize = True
         Label10.Font = New Font("Rockwell", 10.2F)
-        Label10.Location = New Point(408, 195)
+        Label10.Location = New Point(315, 293)
         Label10.Name = "Label10"
-        Label10.Size = New Size(62, 20)
+        Label10.Size = New Size(50, 17)
         Label10.TabIndex = 17
         Label10.Text = "Clases"
         ' 
@@ -118,9 +107,9 @@ Partial Class Matricula
         ' 
         Label9.AutoSize = True
         Label9.Font = New Font("Rockwell", 10.2F)
-        Label9.Location = New Point(19, 195)
+        Label9.Location = New Point(27, 293)
         Label9.Name = "Label9"
-        Label9.Size = New Size(71, 20)
+        Label9.Size = New Size(59, 17)
         Label9.TabIndex = 16
         Label9.Text = "Carrera"
         ' 
@@ -128,90 +117,98 @@ Partial Class Matricula
         ' 
         Label8.AutoSize = True
         Label8.Font = New Font("Rockwell", 10.2F)
-        Label8.Location = New Point(19, 39)
+        Label8.Location = New Point(17, 29)
         Label8.Name = "Label8"
-        Label8.Size = New Size(158, 20)
+        Label8.Size = New Size(130, 17)
         Label8.TabIndex = 15
         Label8.Text = "Cod. Pre-Matricula"
         ' 
         ' TextBox7
         ' 
         TextBox7.Font = New Font("Segoe UI", 9F)
-        TextBox7.Location = New Point(537, 34)
+        TextBox7.Location = New Point(470, 26)
+        TextBox7.Margin = New Padding(3, 2, 3, 2)
         TextBox7.Name = "TextBox7"
-        TextBox7.Size = New Size(265, 27)
+        TextBox7.Size = New Size(232, 23)
         TextBox7.TabIndex = 14
         ' 
         ' DateTimePicker1
         ' 
         DateTimePicker1.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        DateTimePicker1.Location = New Point(199, 115)
+        DateTimePicker1.Location = New Point(174, 86)
+        DateTimePicker1.Margin = New Padding(3, 2, 3, 2)
         DateTimePicker1.Name = "DateTimePicker1"
-        DateTimePicker1.Size = New Size(190, 27)
+        DateTimePicker1.Size = New Size(167, 23)
         DateTimePicker1.TabIndex = 13
         ' 
         ' TextBox6
         ' 
-        TextBox6.Location = New Point(143, 151)
+        TextBox6.Location = New Point(125, 113)
+        TextBox6.Margin = New Padding(3, 2, 3, 2)
         TextBox6.Name = "TextBox6"
-        TextBox6.Size = New Size(246, 30)
+        TextBox6.Size = New Size(216, 26)
         TextBox6.TabIndex = 12
         ' 
         ' TextBox5
         ' 
         TextBox5.Font = New Font("Segoe UI", 9F)
-        TextBox5.Location = New Point(506, 160)
+        TextBox5.Location = New Point(443, 120)
+        TextBox5.Margin = New Padding(3, 2, 3, 2)
         TextBox5.Name = "TextBox5"
-        TextBox5.Size = New Size(296, 27)
+        TextBox5.Size = New Size(260, 23)
         TextBox5.TabIndex = 11
         ' 
         ' TextBox4
         ' 
         TextBox4.Font = New Font("Segoe UI", 9F)
-        TextBox4.Location = New Point(506, 114)
+        TextBox4.Location = New Point(443, 86)
+        TextBox4.Margin = New Padding(3, 2, 3, 2)
         TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(296, 27)
+        TextBox4.Size = New Size(260, 23)
         TextBox4.TabIndex = 10
         ' 
         ' TextBox3
         ' 
-        TextBox3.Location = New Point(143, 76)
+        TextBox3.Location = New Point(125, 57)
+        TextBox3.Margin = New Padding(3, 2, 3, 2)
         TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(246, 30)
+        TextBox3.Size = New Size(216, 26)
         TextBox3.TabIndex = 9
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
         Label2.Font = New Font("Rockwell", 10.2F)
-        Label2.Location = New Point(19, 81)
+        Label2.Location = New Point(17, 61)
         Label2.Name = "Label2"
-        Label2.Size = New Size(81, 20)
+        Label2.Size = New Size(67, 17)
         Label2.TabIndex = 1
         Label2.Text = "Nombres"
         ' 
         ' TextBox2
         ' 
         TextBox2.Font = New Font("Segoe UI", 9F)
-        TextBox2.Location = New Point(506, 76)
+        TextBox2.Location = New Point(443, 57)
+        TextBox2.Margin = New Padding(3, 2, 3, 2)
         TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(296, 27)
+        TextBox2.Size = New Size(260, 23)
         TextBox2.TabIndex = 8
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(183, 31)
+        TextBox1.Location = New Point(160, 23)
+        TextBox1.Margin = New Padding(3, 2, 3, 2)
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(206, 30)
+        TextBox1.Size = New Size(181, 26)
         TextBox1.TabIndex = 7
         ' 
         ' Label7
         ' 
         Label7.AutoSize = True
         Label7.Font = New Font("Rockwell", 10.2F)
-        Label7.Location = New Point(19, 156)
+        Label7.Location = New Point(17, 117)
         Label7.Name = "Label7"
-        Label7.Size = New Size(79, 20)
+        Label7.Size = New Size(64, 17)
         Label7.TabIndex = 6
         Label7.Text = "Teléfono"
         ' 
@@ -219,9 +216,9 @@ Partial Class Matricula
         ' 
         Label6.AutoSize = True
         Label6.Font = New Font("Rockwell", 10.2F)
-        Label6.Location = New Point(408, 160)
+        Label6.Location = New Point(357, 120)
         Label6.Name = "Label6"
-        Label6.Size = New Size(86, 20)
+        Label6.Size = New Size(70, 17)
         Label6.TabIndex = 5
         Label6.Text = "Dirección"
         ' 
@@ -229,9 +226,9 @@ Partial Class Matricula
         ' 
         Label5.AutoSize = True
         Label5.Font = New Font("Rockwell", 10.2F)
-        Label5.Location = New Point(19, 117)
+        Label5.Location = New Point(17, 88)
         Label5.Name = "Label5"
-        Label5.Size = New Size(174, 20)
+        Label5.Size = New Size(143, 17)
         Label5.TabIndex = 4
         Label5.Text = "Fecha de Nacimiento"
         ' 
@@ -239,9 +236,9 @@ Partial Class Matricula
         ' 
         Label4.AutoSize = True
         Label4.Font = New Font("Rockwell", 10.2F)
-        Label4.Location = New Point(408, 124)
+        Label4.Location = New Point(357, 93)
         Label4.Name = "Label4"
-        Label4.Size = New Size(92, 20)
+        Label4.Size = New Size(78, 17)
         Label4.TabIndex = 3
         Label4.Text = "2°Apellido"
         ' 
@@ -249,9 +246,9 @@ Partial Class Matricula
         ' 
         Label3.AutoSize = True
         Label3.Font = New Font("Rockwell", 10.2F)
-        Label3.Location = New Point(408, 81)
+        Label3.Location = New Point(357, 61)
         Label3.Name = "Label3"
-        Label3.Size = New Size(92, 20)
+        Label3.Size = New Size(78, 17)
         Label3.TabIndex = 2
         Label3.Text = "1°Apellido"
         ' 
@@ -259,18 +256,19 @@ Partial Class Matricula
         ' 
         Label1.AutoSize = True
         Label1.Font = New Font("Rockwell", 10.2F)
-        Label1.Location = New Point(408, 39)
+        Label1.Location = New Point(357, 29)
         Label1.Name = "Label1"
-        Label1.Size = New Size(123, 20)
+        Label1.Size = New Size(101, 17)
         Label1.TabIndex = 0
         Label1.Text = "N° Documento"
         ' 
         ' btnatras
         ' 
         btnatras.Image = CType(resources.GetObject("btnatras.Image"), Image)
-        btnatras.Location = New Point(750, 2)
+        btnatras.Location = New Point(656, 2)
+        btnatras.Margin = New Padding(3, 2, 3, 2)
         btnatras.Name = "btnatras"
-        btnatras.Size = New Size(70, 66)
+        btnatras.Size = New Size(61, 50)
         btnatras.TabIndex = 1
         btnatras.UseVisualStyleBackColor = True
         ' 
@@ -278,9 +276,10 @@ Partial Class Matricula
         ' 
         btnnuevo.Image = CType(resources.GetObject("btnnuevo.Image"), Image)
         btnnuevo.ImageAlign = ContentAlignment.TopCenter
-        btnnuevo.Location = New Point(224, 329)
+        btnnuevo.Location = New Point(195, 211)
+        btnnuevo.Margin = New Padding(3, 2, 3, 2)
         btnnuevo.Name = "btnnuevo"
-        btnnuevo.Size = New Size(94, 76)
+        btnnuevo.Size = New Size(82, 57)
         btnnuevo.TabIndex = 2
         btnnuevo.Text = "Nuevo"
         btnnuevo.TextAlign = ContentAlignment.BottomCenter
@@ -290,9 +289,10 @@ Partial Class Matricula
         ' 
         btnguardar.Image = CType(resources.GetObject("btnguardar.Image"), Image)
         btnguardar.ImageAlign = ContentAlignment.TopCenter
-        btnguardar.Location = New Point(324, 329)
+        btnguardar.Location = New Point(283, 211)
+        btnguardar.Margin = New Padding(3, 2, 3, 2)
         btnguardar.Name = "btnguardar"
-        btnguardar.Size = New Size(94, 76)
+        btnguardar.Size = New Size(82, 57)
         btnguardar.TabIndex = 3
         btnguardar.Text = "Guardar"
         btnguardar.TextAlign = ContentAlignment.BottomCenter
@@ -302,9 +302,10 @@ Partial Class Matricula
         ' 
         btnmodificar.Image = CType(resources.GetObject("btnmodificar.Image"), Image)
         btnmodificar.ImageAlign = ContentAlignment.TopCenter
-        btnmodificar.Location = New Point(424, 329)
+        btnmodificar.Location = New Point(371, 211)
+        btnmodificar.Margin = New Padding(3, 2, 3, 2)
         btnmodificar.Name = "btnmodificar"
-        btnmodificar.Size = New Size(94, 76)
+        btnmodificar.Size = New Size(82, 57)
         btnmodificar.TabIndex = 4
         btnmodificar.Text = "Modificar"
         btnmodificar.TextAlign = ContentAlignment.BottomCenter
@@ -314,9 +315,10 @@ Partial Class Matricula
         ' 
         btneliminar.Image = CType(resources.GetObject("btneliminar.Image"), Image)
         btneliminar.ImageAlign = ContentAlignment.TopCenter
-        btneliminar.Location = New Point(524, 329)
+        btneliminar.Location = New Point(459, 211)
+        btneliminar.Margin = New Padding(3, 2, 3, 2)
         btneliminar.Name = "btneliminar"
-        btneliminar.Size = New Size(94, 76)
+        btneliminar.Size = New Size(82, 57)
         btneliminar.TabIndex = 5
         btneliminar.Text = "Eliminar"
         btneliminar.TextAlign = ContentAlignment.BottomCenter
@@ -325,10 +327,11 @@ Partial Class Matricula
         ' DataGridView1
         ' 
         DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Location = New Point(31, 431)
+        DataGridView1.Location = New Point(12, 363)
+        DataGridView1.Margin = New Padding(3, 2, 3, 2)
         DataGridView1.Name = "DataGridView1"
         DataGridView1.RowHeadersWidth = 51
-        DataGridView1.Size = New Size(783, 275)
+        DataGridView1.Size = New Size(685, 253)
         DataGridView1.TabIndex = 6
         ' 
         ' PrintDialog1
@@ -338,17 +341,21 @@ Partial Class Matricula
         ' btnimprimir
         ' 
         btnimprimir.Image = CType(resources.GetObject("btnimprimir.Image"), Image)
-        btnimprimir.Location = New Point(763, 712)
+        btnimprimir.Location = New Point(668, 534)
+        btnimprimir.Margin = New Padding(3, 2, 3, 2)
         btnimprimir.Name = "btnimprimir"
-        btnimprimir.Size = New Size(51, 47)
+        btnimprimir.Size = New Size(45, 35)
         btnimprimir.TabIndex = 7
         btnimprimir.UseVisualStyleBackColor = True
         ' 
         ' Matricula
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(832, 786)
+        ClientSize = New Size(728, 610)
+        Controls.Add(Label10)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label9)
         Controls.Add(btnimprimir)
         Controls.Add(DataGridView1)
         Controls.Add(btneliminar)
@@ -358,6 +365,7 @@ Partial Class Matricula
         Controls.Add(btnatras)
         Controls.Add(GroupBox1)
         FormBorderStyle = FormBorderStyle.None
+        Margin = New Padding(3, 2, 3, 2)
         Name = "Matricula"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Matricula"
@@ -365,6 +373,7 @@ Partial Class Matricula
         GroupBox1.PerformLayout()
         CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents GroupBox1 As GroupBox
@@ -392,7 +401,6 @@ Partial Class Matricula
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents Label10 As Label
     Friend WithEvents Label9 As Label
-    Friend WithEvents CheckedListBox1 As CheckedListBox
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents PrintDialog1 As PrintDialog
     Friend WithEvents btnimprimir As Button
