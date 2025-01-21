de5ec9b53f98530581334adaaaec8e73d7162275
carlosmoncada04 <carlosfeli0312@gmail.com>
2025-01-20 18:49:38 -0600

    termino

--- Carreras.Designer.vb
+++ Carreras.Designer.vb
@@ -34,20 +34,7 @@ Partial Class Carreras
         CheckBox3 = New CheckBox()
         CheckBox2 = New CheckBox()
         CheckBox1 = New CheckBox()
-        TextBox5 = New TextBox()
-        Label6 = New Label()
-        TextBox3 = New TextBox()
-        TextBox1 = New TextBox()
-        Label5 = New Label()
         Label1 = New Label()
-        TextBox6 = New TextBox()
-        TextBox4 = New TextBox()
-        TextBox2 = New TextBox()
-        Label7 = New Label()
-        Label4 = New Label()
-        Label3 = New Label()
-        Label2 = New Label()
-        Button2 = New Button()
         Label8 = New Label()
         ListBox1 = New ListBox()
         PictureBox1 = New PictureBox()
@@ -55,27 +42,25 @@ Partial Class Carreras
         btnmodificar = New Button()
         btnguardar = New Button()
         Button1 = New Button()
+        Button2 = New Button()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' ListMenu
         ' 
         ListMenu.FormattingEnabled = True
-        ListMenu.ItemHeight = 25
         ListMenu.Items.AddRange(New Object() {"Ingeniería en Sistemas", " Medicina", " Derecho", " Psicología", " Arquitectura", " Administración de Empresas", " Diseño Gráfico", " Contaduría Pública", " Ingeniería Industrial", "", "Comunicación Social"})
-        ListMenu.Location = New Point(439, 353)
-        ListMenu.Margin = New Padding(4)
+        ListMenu.Location = New Point(390, 207)
         ListMenu.Name = "ListMenu"
-        ListMenu.Size = New Size(542, 304)
+        ListMenu.Size = New Size(434, 244)
         ListMenu.TabIndex = 62
         ' 
         ' CheckBox10
         ' 
         CheckBox10.AutoSize = True
-        CheckBox10.Location = New Point(417, 610)
-        CheckBox10.Margin = New Padding(4)
+        CheckBox10.Location = New Point(373, 413)
         CheckBox10.Name = "CheckBox10"
-        CheckBox10.Size = New Size(134, 29)
+        CheckBox10.Size = New Size(111, 24)
         CheckBox10.TabIndex = 72
         CheckBox10.Text = "CheckBox10"
         CheckBox10.UseVisualStyleBackColor = True
@@ -83,10 +68,9 @@ Partial Class Carreras
         ' CheckBox9
         ' 
         CheckBox9.AutoSize = True
-        CheckBox9.Location = New Point(417, 556)
-        CheckBox9.Margin = New Padding(4)
+        CheckBox9.Location = New Point(373, 370)
         CheckBox9.Name = "CheckBox9"
-        CheckBox9.Size = New Size(124, 29)
+        CheckBox9.Size = New Size(103, 24)
         CheckBox9.TabIndex = 71
         CheckBox9.Text = "CheckBox9"
         CheckBox9.UseVisualStyleBackColor = True
@@ -94,10 +78,9 @@ Partial Class Carreras
         ' CheckBox8
         ' 
         CheckBox8.AutoSize = True
-        CheckBox8.Location = New Point(417, 530)
-        CheckBox8.Margin = New Padding(4)
+        CheckBox8.Location = New Point(373, 349)
         CheckBox8.Name = "CheckBox8"
-        CheckBox8.Size = New Size(124, 29)
+        CheckBox8.Size = New Size(103, 24)
         CheckBox8.TabIndex = 70
         CheckBox8.Text = "CheckBox8"
         CheckBox8.UseVisualStyleBackColor = True
@@ -105,10 +88,9 @@ Partial Class Carreras
         ' CheckBox7
         ' 
         CheckBox7.AutoSize = True
-        CheckBox7.Location = New Point(417, 506)
-        CheckBox7.Margin = New Padding(4)
+        CheckBox7.Location = New Point(373, 330)
         CheckBox7.Name = "CheckBox7"
-        CheckBox7.Size = New Size(124, 29)
+        CheckBox7.Size = New Size(103, 24)
         CheckBox7.TabIndex = 69
         CheckBox7.Text = "CheckBox7"
         CheckBox7.UseVisualStyleBackColor = True
@@ -116,10 +98,9 @@ Partial Class Carreras
         ' CheckBox6
         ' 
         CheckBox6.AutoSize = True
-        CheckBox6.Location = New Point(417, 480)
-        CheckBox6.Margin = New Padding(4)
+        CheckBox6.Location = New Point(373, 309)
         CheckBox6.Name = "CheckBox6"
-        CheckBox6.Size = New Size(124, 29)
+        CheckBox6.Size = New Size(103, 24)
         CheckBox6.TabIndex = 68
         CheckBox6.Text = "CheckBox6"
         CheckBox6.UseVisualStyleBackColor = True
@@ -127,10 +108,9 @@ Partial Class Carreras
         ' CheckBox5
         ' 
         CheckBox5.AutoSize = True
-        CheckBox5.Location = New Point(417, 456)
-        CheckBox5.Margin = New Padding(4)
+        CheckBox5.Location = New Point(373, 290)
         CheckBox5.Name = "CheckBox5"
-        CheckBox5.Size = New Size(124, 29)
+        CheckBox5.Size = New Size(103, 24)
         CheckBox5.TabIndex = 67
         CheckBox5.Text = "CheckBox5"
         CheckBox5.UseVisualStyleBackColor = True
@@ -138,10 +118,9 @@ Partial Class Carreras
         ' CheckBox4
         ' 
         CheckBox4.AutoSize = True
-        CheckBox4.Location = New Point(417, 431)
-        CheckBox4.Margin = New Padding(4)
+        CheckBox4.Location = New Point(373, 270)
         CheckBox4.Name = "CheckBox4"
-        CheckBox4.Size = New Size(124, 29)
+        CheckBox4.Size = New Size(103, 24)
         CheckBox4.TabIndex = 66
         CheckBox4.Text = "CheckBox4"
         CheckBox4.UseVisualStyleBackColor = True
@@ -149,10 +128,9 @@ Partial Class Carreras
         ' CheckBox3
         ' 
         CheckBox3.AutoSize = True
-        CheckBox3.Location = New Point(417, 405)
-        CheckBox3.Margin = New Padding(4)
+        CheckBox3.Location = New Point(373, 249)
         CheckBox3.Name = "CheckBox3"
-        CheckBox3.Size = New Size(124, 29)
+        CheckBox3.Size = New Size(103, 24)
         CheckBox3.TabIndex = 65
         CheckBox3.Text = "CheckBox3"
         CheckBox3.UseVisualStyleBackColor = True
@@ -160,10 +138,9 @@ Partial Class Carreras
         ' CheckBox2
         ' 
         CheckBox2.AutoSize = True
-        CheckBox2.Location = New Point(417, 379)
-        CheckBox2.Margin = New Padding(4)
+        CheckBox2.Location = New Point(373, 228)
         CheckBox2.Name = "CheckBox2"
-        CheckBox2.Size = New Size(124, 29)
+        CheckBox2.Size = New Size(103, 24)
         CheckBox2.TabIndex = 64
         CheckBox2.Text = "CheckBox2"
         CheckBox2.UseVisualStyleBackColor = True
@@ -171,183 +148,47 @@ Partial Class Carreras
         ' CheckBox1
         ' 
         CheckBox1.AutoSize = True
-        CheckBox1.Location = New Point(417, 353)
-        CheckBox1.Margin = New Padding(4)
+        CheckBox1.Location = New Point(373, 207)
         CheckBox1.Name = "CheckBox1"
-        CheckBox1.Size = New Size(124, 29)
+        CheckBox1.Size = New Size(103, 24)
         CheckBox1.TabIndex = 63
         CheckBox1.Text = "CheckBox1"
         CheckBox1.UseVisualStyleBackColor = True
-        ' 
-        ' TextBox5
-        ' 
-        TextBox5.Font = New Font("Segoe UI", 9F)
-        TextBox5.Location = New Point(599, 296)
-        TextBox5.Margin = New Padding(4)
-        TextBox5.Name = "TextBox5"
-        TextBox5.Size = New Size(369, 31)
-        TextBox5.TabIndex = 61
-        ' 
-        ' Label6
-        ' 
-        Label6.AutoSize = True
-        Label6.Font = New Font("Rockwell", 10.2F)
-        Label6.Location = New Point(470, 304)
-        Label6.Margin = New Padding(4, 0, 4, 0)
-        Label6.Name = "Label6"
-        Label6.Size = New Size(104, 23)
-        Label6.TabIndex = 60
-        Label6.Text = "Dirección"
-        ' 
-        ' TextBox3
-        ' 
-        TextBox3.Font = New Font("Segoe UI", 9F)
-        TextBox3.Location = New Point(599, 203)
-        TextBox3.Margin = New Padding(4)
-        TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(369, 31)
-        TextBox3.TabIndex = 59
-        ' 
-        ' TextBox1
-        ' 
-        TextBox1.Font = New Font("Segoe UI", 9F)
-        TextBox1.Location = New Point(599, 118)
-        TextBox1.Margin = New Padding(4)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(369, 31)
-        TextBox1.TabIndex = 58
-        ' 
-        ' Label5
-        ' 
-        Label5.AutoSize = True
-        Label5.Font = New Font("Rockwell", 10.2F)
-        Label5.Location = New Point(462, 118)
-        Label5.Margin = New Padding(4, 0, 4, 0)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(118, 23)
-        Label5.TabIndex = 57
-        Label5.Text = "2°Nombres"
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
         Label1.Font = New Font("Rockwell", 24F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label1.ForeColor = Color.Black
-        Label1.Location = New Point(462, 3)
-        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Location = New Point(390, 95)
         Label1.Name = "Label1"
-        Label1.Size = New Size(497, 54)
+        Label1.Size = New Size(415, 46)
         Label1.TabIndex = 55
         Label1.Text = "Carreras Disponibles"
-        ' 
-        ' TextBox6
-        ' 
-        TextBox6.Location = New Point(599, 254)
-        TextBox6.Margin = New Padding(4)
-        TextBox6.Name = "TextBox6"
-        TextBox6.Size = New Size(369, 31)
-        TextBox6.TabIndex = 54
-        ' 
-        ' TextBox4
-        ' 
-        TextBox4.Font = New Font("Segoe UI", 9F)
-        TextBox4.Location = New Point(599, 159)
-        TextBox4.Margin = New Padding(4)
-        TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(369, 31)
-        TextBox4.TabIndex = 53
-        ' 
-        ' TextBox2
-        ' 
-        TextBox2.Font = New Font("Segoe UI", 9F)
-        TextBox2.Location = New Point(599, 67)
-        TextBox2.Margin = New Padding(4)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(369, 31)
-        TextBox2.TabIndex = 52
-        ' 
-        ' Label7
-        ' 
-        Label7.AutoSize = True
-        Label7.Font = New Font("Rockwell", 10.2F)
-        Label7.Location = New Point(480, 259)
-        Label7.Margin = New Padding(4, 0, 4, 0)
-        Label7.Name = "Label7"
-        Label7.Size = New Size(94, 23)
-        Label7.TabIndex = 51
-        Label7.Text = "Teléfono"
-        ' 
-        ' Label4
-        ' 
-        Label4.AutoSize = True
-        Label4.Font = New Font("Rockwell", 10.2F)
-        Label4.Location = New Point(462, 212)
-        Label4.Margin = New Padding(4, 0, 4, 0)
-        Label4.Name = "Label4"
-        Label4.Size = New Size(112, 23)
-        Label4.TabIndex = 50
-        Label4.Text = "2°Apellido"
-        ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.Font = New Font("Rockwell", 10.2F)
-        Label3.Location = New Point(462, 168)
-        Label3.Margin = New Padding(4, 0, 4, 0)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(112, 23)
-        Label3.TabIndex = 49
-        Label3.Text = "1°Apellido"
-        ' 
-        ' Label2
-        ' 
-        Label2.AutoSize = True
-        Label2.Font = New Font("Rockwell", 10.2F)
-        Label2.Location = New Point(462, 70)
-        Label2.Margin = New Padding(4, 0, 4, 0)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(118, 23)
-        Label2.TabIndex = 48
-        Label2.Text = "1°Nombres"
-        ' 
-        ' Button2
-        ' 
-        Button2.BackColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
-        Button2.Location = New Point(408, 27)
-        Button2.Margin = New Padding(4)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(698, 745)
-        Button2.TabIndex = 56
-        Button2.UseVisualStyleBackColor = False
         ' 
         ' Label8
         ' 
         Label8.AutoSize = True
         Label8.Font = New Font("Rockwell", 10.2F)
-        Label8.Location = New Point(188, 28)
-        Label8.Margin = New Padding(4, 0, 4, 0)
+        Label8.Location = New Point(150, 22)
         Label8.Name = "Label8"
-        Label8.Size = New Size(166, 23)
+        Label8.Size = New Size(135, 20)
         Label8.TabIndex = 74
         Label8.Text = "Carrera Elegida"
         ' 
         ' ListBox1
         ' 
         ListBox1.FormattingEnabled = True
-        ListBox1.ItemHeight = 25
-        ListBox1.Location = New Point(172, 66)
-        ListBox1.Margin = New Padding(4)
+        ListBox1.Location = New Point(138, 53)
         ListBox1.Name = "ListBox1"
-        ListBox1.Size = New Size(196, 154)
+        ListBox1.Size = New Size(158, 124)
         ListBox1.TabIndex = 73
         ' 
         ' PictureBox1
         ' 
-        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
-        PictureBox1.Location = New Point(16, 17)
-        PictureBox1.Margin = New Padding(4)
+        PictureBox1.Location = New Point(13, 14)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(83, 81)
+        PictureBox1.Size = New Size(66, 65)
         PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox1.TabIndex = 79
         PictureBox1.TabStop = False
@@ -356,10 +197,9 @@ Partial Class Carreras
         ' 
         btneliminar.Image = CType(resources.GetObject("btneliminar.Image"), Image)
         btneliminar.ImageAlign = ContentAlignment.TopCenter
-        btneliminar.Location = New Point(16, 591)
-        btneliminar.Margin = New Padding(4)
+        btneliminar.Location = New Point(13, 473)
         btneliminar.Name = "btneliminar"
-        btneliminar.Size = New Size(94, 101)
+        btneliminar.Size = New Size(75, 81)
         btneliminar.TabIndex = 78
         btneliminar.Text = "Eliminar"
         btneliminar.TextAlign = ContentAlignment.BottomCenter
@@ -369,10 +209,9 @@ Partial Class Carreras
         ' 
         btnmodificar.Image = CType(resources.GetObject("btnmodificar.Image"), Image)
         btnmodificar.ImageAlign = ContentAlignment.TopCenter
-        btnmodificar.Location = New Point(16, 377)
-        btnmodificar.Margin = New Padding(4)
+        btnmodificar.Location = New Point(13, 302)
         btnmodificar.Name = "btnmodificar"
-        btnmodificar.Size = New Size(94, 101)
+        btnmodificar.Size = New Size(75, 81)
         btnmodificar.TabIndex = 77
         btnmodificar.Text = "Limpiar"
         btnmodificar.TextAlign = ContentAlignment.BottomCenter
@@ -382,10 +221,9 @@ Partial Class Carreras
         ' 
         btnguardar.Image = CType(resources.GetObject("btnguardar.Image"), Image)
         btnguardar.ImageAlign = ContentAlignment.TopCenter
-        btnguardar.Location = New Point(16, 178)
-        btnguardar.Margin = New Padding(4)
+        btnguardar.Location = New Point(13, 142)
         btnguardar.Name = "btnguardar"
-        btnguardar.Size = New Size(94, 101)
+        btnguardar.Size = New Size(75, 81)
         btnguardar.TabIndex = 76
         btnguardar.Text = "Guardar"
         btnguardar.TextAlign = ContentAlignment.BottomCenter
@@ -396,17 +234,25 @@ Partial Class Carreras
         Button1.BackColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
         Button1.ForeColor = Color.Black
         Button1.Location = New Point(-1, -2)
-        Button1.Margin = New Padding(4)
         Button1.Name = "Button1"
-        Button1.Size = New Size(120, 788)
+        Button1.Size = New Size(96, 630)
         Button1.TabIndex = 75
         Button1.UseVisualStyleBackColor = False
         ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        Button2.Location = New Point(326, 22)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(558, 596)
+        Button2.TabIndex = 56
+        Button2.UseVisualStyleBackColor = False
+        ' 
         ' Carreras
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1176, 782)
+        ClientSize = New Size(941, 626)
         Controls.Add(PictureBox1)
         Controls.Add(btneliminar)
         Controls.Add(btnmodificar)
@@ -425,22 +271,9 @@ Partial Class Carreras
         Controls.Add(CheckBox3)
         Controls.Add(CheckBox2)
         Controls.Add(CheckBox1)
-        Controls.Add(TextBox5)
-        Controls.Add(Label6)
-        Controls.Add(TextBox3)
-        Controls.Add(TextBox1)
-        Controls.Add(Label5)
         Controls.Add(Label1)
-        Controls.Add(TextBox6)
-        Controls.Add(TextBox4)
-        Controls.Add(TextBox2)
-        Controls.Add(Label7)
-        Controls.Add(Label4)
-        Controls.Add(Label3)
-        Controls.Add(Label2)
         Controls.Add(Button2)
         FormBorderStyle = FormBorderStyle.None
-        Margin = New Padding(4, 4, 4, 4)
         Name = "Carreras"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Carreras"
@@ -460,20 +293,7 @@ Partial Class Carreras
     Friend WithEvents CheckBox3 As CheckBox
     Friend WithEvents CheckBox2 As CheckBox
     Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents TextBox5 As TextBox
-    Friend WithEvents Label6 As Label
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Label5 As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox6 As TextBox
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Label7 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Button2 As Button
     Friend WithEvents Label8 As Label
     Friend WithEvents ListBox1 As ListBox
     Friend WithEvents PictureBox1 As PictureBox
@@ -481,4 +301,5 @@ Partial Class Carreras
     Friend WithEvents btnmodificar As Button
     Friend WithEvents btnguardar As Button
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
