2a52bc8fe52da5867571de0bf0c06789b45e3206
Emmanuel Castillo <emmanuelfernando930@gmail.com>
2025-01-22 11:58:13 -0600

    cambio de color

--- Estudiantes.Designer.vb
+++ Estudiantes.Designer.vb
@@ -53,21 +53,19 @@ Partial Class Estudiantes
         ' DataGridView1
         ' 
         DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Location = New Point(57, 515)
-        DataGridView1.Margin = New Padding(4)
+        DataGridView1.Location = New Point(46, 412)
         DataGridView1.Name = "DataGridView1"
         DataGridView1.RowHeadersWidth = 51
-        DataGridView1.Size = New Size(979, 424)
+        DataGridView1.Size = New Size(783, 339)
         DataGridView1.TabIndex = 13
         ' 
         ' btneliminar
         ' 
         btneliminar.Image = CType(resources.GetObject("btneliminar.Image"), Image)
         btneliminar.ImageAlign = ContentAlignment.TopCenter
-        btneliminar.Location = New Point(689, 380)
-        btneliminar.Margin = New Padding(4)
+        btneliminar.Location = New Point(551, 304)
         btneliminar.Name = "btneliminar"
-        btneliminar.Size = New Size(118, 95)
+        btneliminar.Size = New Size(94, 76)
         btneliminar.TabIndex = 12
         btneliminar.Text = "Eliminar"
         btneliminar.TextAlign = ContentAlignment.BottomCenter
@@ -77,10 +75,9 @@ Partial Class Estudiantes
         ' 
         btnmodificar.Image = CType(resources.GetObject("btnmodificar.Image"), Image)
         btnmodificar.ImageAlign = ContentAlignment.TopCenter
-        btnmodificar.Location = New Point(564, 380)
-        btnmodificar.Margin = New Padding(4)
+        btnmodificar.Location = New Point(451, 304)
         btnmodificar.Name = "btnmodificar"
-        btnmodificar.Size = New Size(118, 95)
+        btnmodificar.Size = New Size(94, 76)
         btnmodificar.TabIndex = 11
         btnmodificar.Text = "Modificar"
         btnmodificar.TextAlign = ContentAlignment.BottomCenter
@@ -90,10 +87,9 @@ Partial Class Estudiantes
         ' 
         btnguardar.Image = CType(resources.GetObject("btnguardar.Image"), Image)
         btnguardar.ImageAlign = ContentAlignment.TopCenter
-        btnguardar.Location = New Point(439, 380)
-        btnguardar.Margin = New Padding(4)
+        btnguardar.Location = New Point(351, 304)
         btnguardar.Name = "btnguardar"
-        btnguardar.Size = New Size(118, 95)
+        btnguardar.Size = New Size(94, 76)
         btnguardar.TabIndex = 10
         btnguardar.Text = "Guardar"
         btnguardar.TextAlign = ContentAlignment.BottomCenter
@@ -103,10 +99,9 @@ Partial Class Estudiantes
         ' 
         btnnuevo.Image = CType(resources.GetObject("btnnuevo.Image"), Image)
         btnnuevo.ImageAlign = ContentAlignment.TopCenter
-        btnnuevo.Location = New Point(314, 380)
-        btnnuevo.Margin = New Padding(4)
+        btnnuevo.Location = New Point(251, 304)
         btnnuevo.Name = "btnnuevo"
-        btnnuevo.Size = New Size(118, 95)
+        btnnuevo.Size = New Size(94, 76)
         btnnuevo.TabIndex = 9
         btnnuevo.Text = "Nuevo"
         btnnuevo.TextAlign = ContentAlignment.BottomCenter
@@ -115,10 +110,9 @@ Partial Class Estudiantes
         ' btnatras
         ' 
         btnatras.Image = CType(resources.GetObject("btnatras.Image"), Image)
-        btnatras.Location = New Point(1001, 10)
-        btnatras.Margin = New Padding(4)
+        btnatras.Location = New Point(801, 8)
         btnatras.Name = "btnatras"
-        btnatras.Size = New Size(88, 82)
+        btnatras.Size = New Size(70, 66)
         btnatras.TabIndex = 8
         btnatras.UseVisualStyleBackColor = True
         ' 
@@ -142,11 +136,9 @@ Partial Class Estudiantes
         GroupBox1.Controls.Add(Label1)
         GroupBox1.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         GroupBox1.ForeColor = Color.Black
-        GroupBox1.Location = New Point(66, 100)
-        GroupBox1.Margin = New Padding(4)
+        GroupBox1.Location = New Point(53, 80)
         GroupBox1.Name = "GroupBox1"
-        GroupBox1.Padding = New Padding(4)
-        GroupBox1.Size = New Size(1010, 240)
+        GroupBox1.Size = New Size(808, 192)
         GroupBox1.TabIndex = 7
         GroupBox1.TabStop = False
         GroupBox1.Text = "Información del estudiante"
@@ -155,90 +147,80 @@ Partial Class Estudiantes
         ' 
         Label8.AutoSize = True
         Label8.Font = New Font("Rockwell", 10.2F)
-        Label8.Location = New Point(25, 50)
-        Label8.Margin = New Padding(4, 0, 4, 0)
+        Label8.Location = New Point(20, 40)
         Label8.Name = "Label8"
-        Label8.Size = New Size(147, 23)
+        Label8.Size = New Size(121, 20)
         Label8.TabIndex = 15
         Label8.Text = "Cod.Matricula"
         ' 
         ' TextBox7
         ' 
         TextBox7.Font = New Font("Segoe UI", 9F)
-        TextBox7.Location = New Point(667, 42)
-        TextBox7.Margin = New Padding(4)
+        TextBox7.Location = New Point(534, 34)
         TextBox7.Name = "TextBox7"
-        TextBox7.Size = New Size(330, 31)
+        TextBox7.Size = New Size(265, 27)
         TextBox7.TabIndex = 14
         ' 
         ' DateTimePicker1
         ' 
         DateTimePicker1.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        DateTimePicker1.Location = New Point(249, 144)
-        DateTimePicker1.Margin = New Padding(4)
+        DateTimePicker1.Location = New Point(199, 115)
         DateTimePicker1.Name = "DateTimePicker1"
-        DateTimePicker1.Size = New Size(236, 31)
+        DateTimePicker1.Size = New Size(190, 27)
         DateTimePicker1.TabIndex = 13
         ' 
         ' TextBox6
         ' 
-        TextBox6.Location = New Point(179, 189)
-        TextBox6.Margin = New Padding(4)
+        TextBox6.Location = New Point(143, 151)
         TextBox6.Name = "TextBox6"
-        TextBox6.Size = New Size(306, 35)
+        TextBox6.Size = New Size(246, 30)
         TextBox6.TabIndex = 12
         ' 
         ' TextBox5
         ' 
         TextBox5.Font = New Font("Segoe UI", 9F)
-        TextBox5.Location = New Point(632, 200)
-        TextBox5.Margin = New Padding(4)
+        TextBox5.Location = New Point(506, 160)
         TextBox5.Name = "TextBox5"
-        TextBox5.Size = New Size(369, 31)
+        TextBox5.Size = New Size(296, 27)
         TextBox5.TabIndex = 11
         ' 
         ' TextBox4
         ' 
         TextBox4.Font = New Font("Segoe UI", 9F)
-        TextBox4.Location = New Point(632, 142)
-        TextBox4.Margin = New Padding(4)
+        TextBox4.Location = New Point(506, 114)
         TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(369, 31)
+        TextBox4.Size = New Size(296, 27)
         TextBox4.TabIndex = 10
         ' 
         ' TextBox3
         ' 
-        TextBox3.Location = New Point(179, 95)
-        TextBox3.Margin = New Padding(4)
+        TextBox3.Location = New Point(143, 76)
         TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(306, 35)
+        TextBox3.Size = New Size(246, 30)
         TextBox3.TabIndex = 9
         ' 
         ' TextBox2
         ' 
         TextBox2.Font = New Font("Segoe UI", 9F)
-        TextBox2.Location = New Point(632, 95)
-        TextBox2.Margin = New Padding(4)
+        TextBox2.Location = New Point(506, 76)
         TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(369, 31)
+        TextBox2.Size = New Size(296, 27)
         TextBox2.TabIndex = 8
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(229, 39)
-        TextBox1.Margin = New Padding(4)
+        TextBox1.Location = New Point(183, 31)
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(256, 35)
+        TextBox1.Size = New Size(206, 30)
         TextBox1.TabIndex = 7
         ' 
         ' Label7
         ' 
         Label7.AutoSize = True
         Label7.Font = New Font("Rockwell", 10.2F)
-        Label7.Location = New Point(25, 196)
-        Label7.Margin = New Padding(4, 0, 4, 0)
+        Label7.Location = New Point(20, 157)
         Label7.Name = "Label7"
-        Label7.Size = New Size(94, 23)
+        Label7.Size = New Size(79, 20)
         Label7.TabIndex = 6
         Label7.Text = "Teléfono"
         ' 
@@ -246,10 +228,9 @@ Partial Class Estudiantes
         ' 
         Label6.AutoSize = True
         Label6.Font = New Font("Rockwell", 10.2F)
-        Label6.Location = New Point(511, 201)
-        Label6.Margin = New Padding(4, 0, 4, 0)
+        Label6.Location = New Point(409, 161)
         Label6.Name = "Label6"
-        Label6.Size = New Size(104, 23)
+        Label6.Size = New Size(86, 20)
         Label6.TabIndex = 5
         Label6.Text = "Dirección"
         ' 
@@ -257,10 +238,9 @@ Partial Class Estudiantes
         ' 
         Label5.AutoSize = True
         Label5.Font = New Font("Rockwell", 10.2F)
-        Label5.Location = New Point(25, 147)
-        Label5.Margin = New Padding(4, 0, 4, 0)
+        Label5.Location = New Point(20, 118)
         Label5.Name = "Label5"
-        Label5.Size = New Size(212, 23)
+        Label5.Size = New Size(174, 20)
         Label5.TabIndex = 4
         Label5.Text = "Fecha de Nacimiento"
         ' 
@@ -268,10 +248,9 @@ Partial Class Estudiantes
         ' 
         Label4.AutoSize = True
         Label4.Font = New Font("Rockwell", 10.2F)
-        Label4.Location = New Point(511, 156)
-        Label4.Margin = New Padding(4, 0, 4, 0)
+        Label4.Location = New Point(409, 125)
         Label4.Name = "Label4"
-        Label4.Size = New Size(112, 23)
+        Label4.Size = New Size(92, 20)
         Label4.TabIndex = 3
         Label4.Text = "2°Apellido"
         ' 
@@ -279,10 +258,9 @@ Partial Class Estudiantes
         ' 
         Label3.AutoSize = True
         Label3.Font = New Font("Rockwell", 10.2F)
-        Label3.Location = New Point(25, 102)
-        Label3.Margin = New Padding(4, 0, 4, 0)
+        Label3.Location = New Point(20, 82)
         Label3.Name = "Label3"
-        Label3.Size = New Size(112, 23)
+        Label3.Size = New Size(92, 20)
         Label3.TabIndex = 2
         Label3.Text = "1°Apellido"
         ' 
@@ -290,10 +268,9 @@ Partial Class Estudiantes
         ' 
         Label2.AutoSize = True
         Label2.Font = New Font("Rockwell", 10.2F)
-        Label2.Location = New Point(511, 102)
-        Label2.Margin = New Padding(4, 0, 4, 0)
+        Label2.Location = New Point(409, 82)
         Label2.Name = "Label2"
-        Label2.Size = New Size(99, 23)
+        Label2.Size = New Size(81, 20)
         Label2.TabIndex = 1
         Label2.Text = "Nombres"
         ' 
@@ -301,18 +278,18 @@ Partial Class Estudiantes
         ' 
         Label1.AutoSize = True
         Label1.Font = New Font("Rockwell", 10.2F)
-        Label1.Location = New Point(511, 50)
-        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Location = New Point(409, 40)
         Label1.Name = "Label1"
-        Label1.Size = New Size(148, 23)
+        Label1.Size = New Size(123, 20)
         Label1.TabIndex = 0
         Label1.Text = "N° Documento"
         ' 
         ' Estudiantes
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1125, 952)
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(900, 762)
         Controls.Add(DataGridView1)
         Controls.Add(btneliminar)
         Controls.Add(btnmodificar)
@@ -320,6 +297,7 @@ Partial Class Estudiantes
         Controls.Add(btnnuevo)
         Controls.Add(btnatras)
         Controls.Add(GroupBox1)
+        Margin = New Padding(2, 2, 2, 2)
         Name = "Estudiantes"
         Text = "Estudiantes"
         CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
