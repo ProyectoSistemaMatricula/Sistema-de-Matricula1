9e48741847ff0fab95ac6b983852ddb0b9bcc1da
DEBORA RAQUEL RAMIREZ MARTINEZ <debora.ramirez@uph.edu.hn>
2025-01-26 22:42:59 -0600

    j

--- Login.vb
+++ Login.vb
@@ -19,4 +19,8 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs)
 
     End Sub
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
